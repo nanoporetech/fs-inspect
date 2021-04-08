a238e30324d2d8e60595eb2458cabdf06a0bb676
Iain Shorter <iain.shorter@nanoporetech.com>
2021-04-08 09:35:30 +0100

    ensure directories have a size of 0

--- src/FileInfo.ts
+++ src/FileInfo.ts
@@ -1,19 +1,20 @@
 import path from 'path';
 import fs from 'fs';
+
 import type { FileInfo } from './FileInfo.type';
 
 export async function makeFileInfo(root: string, relative: string): Promise<FileInfo> {
   const absolute = path.join(root, relative);
-  const { base, name, ext } = path.parse(relative);
+  const { base, name, ext } = path.parse(absolute);
   const info = await fs.promises.stat(absolute);
   const { size, birthtimeMs: created, mtimeMs: modified } = info;
-
+  const isDirectory = info.isDirectory();
   return {
-    isDirectory: info.isDirectory(),
+    isDirectory,
     hidden: name.startsWith('.'),
     relative,
     absolute,
-    size,
+    size: isDirectory ? 0 : size,
     base,
     name,
     ext,
