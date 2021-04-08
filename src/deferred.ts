d00f858bdb183953585dcfa0d517a21c53681a02
Iain Shorter <iain.shorter@nanoporetech.com>
2021-04-08 09:33:40 +0100

    simplify deferred logic (requires a ts ignore)

--- src/deferred.ts
+++ src/deferred.ts
@@ -2,17 +2,15 @@ export function deferred<T> (): { promise: Promise<T>; resolve: (v: T) => void; 
   // we unfortunately have to set dummy values here, assigning to null causes the
   // checker to strip the real type and if we define later the compiler produces
   // a "not yet defined style error"
-  let resolve: ((v: T) => void) = () => {};
-  let reject: ((v: unknown) => void) = () => {};
+  let resolve: ((v: T) => void);
+  let reject: ((v: unknown) => void);
   
   const promise = new Promise<T>((res, rej) => {
     resolve = res;
     reject = rej;
   });
-
-  return {
-    promise,
-    resolve,
-    reject
-  };
+  
+  // resolve/reject are "potentially" not assigned to, but always are
+  // @ts-ignore 
+  return { promise, resolve, reject };
 }
