17ac7beaaac88b19e730a1bd51145425f5412b78
Iain Shorter <iain.shorter@nanoporetech.com>
2021-04-09 18:16:44 +0100

    add common filter for extensions

---
+++ src/common_filters/extension.test.ts
@@ -0,0 +1,19 @@
+import { filterByExtension } from './extension';
+import { createInspector } from '../inspector';
+
+describe('common filter: extension', () => {
+  it('validates extensions', () => {
+    expect(() => filterByExtension(['hello.world'])).toThrowError('Incorrectly formatted extension "hello.world"');
+  });
+  it('normalises start', () => {
+    const filter = filterByExtension(['jpg', '.png']);
+    expect(filter({ ext: ''})).toBeFalsy();
+    expect(filter({ ext: '.jpg'})).toBeTruthy();
+    expect(filter({ ext: '.png'})).toBeTruthy();
+    expect(filter({ ext: 'png'})).toBeFalsy();
+    expect(filter({ ext: '.txt'})).toBeFalsy();
+  });
+  it('is accepted by createInspector', () => {
+    createInspector({ filter: filterByExtension([ 'gz' ])});
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export type { Inspector, InspectorOptions } from './inspector.type';
 export type { FileInfo } from './FileInfo.type';
 export { createInspector } from './inspector';
+export { filterByExtension } from './common_filters/extension'
