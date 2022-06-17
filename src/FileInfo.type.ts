7a40713c865b753baf2a9c6cf479661e61688e8b
Iain Shorter <iain.shorter@nanoporetech.com>
2022-06-17 16:30:25 +0100

    Introduce BasicFileInfo type which can be inferred without FileStat

--- src/FileInfo.type.ts
+++ src/FileInfo.type.ts
@@ -1,8 +1,11 @@
-export interface FileInfo {
+export interface BasicFileInfo {
   isDirectory: boolean;
   relative: string;
   absolute: string;
   hidden: boolean;
+}
+
+export interface FileInfo extends BasicFileInfo {
   size: number;
   base: string;
   name: string;
